45be358dc0899fd0b12aed658c263b0d7efabda9
zuofeng-pc <feng.zuo@bluestonehk.com>
2016-06-20 15:44:08 +0800

    使用onTouchEvent回调方法让小球跟着手指移动

---
+++ app/src/main/java/com/example/user_zf/scandroid/myview/HandBall.java
@@ -0,0 +1,52 @@
+package com.example.user_zf.scandroid.myview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.support.v4.content.ContextCompat;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.example.user_zf.scandroid.R;
+
+/**
+ * Created by user_zf on 16/6/20.
+ * 跟随手移动的小球
+ */
+public class HandBall extends View{
+
+    Paint paint;
+    float currentX;
+    float currentY;
+
+    public HandBall(Context context){
+        super(context);
+        paint = new Paint();
+        currentX = 50;
+        currentY = 50;
+    }
+
+    public HandBall(Context context, AttributeSet atts){
+        super(context, atts);
+        paint = new Paint();
+        currentX = 50;
+        currentY = 50;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        paint.setColor(ContextCompat.getColor(getContext(), R.color.colorAccent));
+        canvas.drawCircle(currentX, currentY, 30, paint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        super.onTouchEvent(event);
+        currentX = event.getX();
+        currentY = event.getY();
+        invalidate();//重绘
+        return true;
+    }
+}
